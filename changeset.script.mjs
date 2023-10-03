0656b7593322671edd9e76bbab5cbf96751dd477
Darren Li <darrenli@hipagesgroup.com.au>
2023-10-04 10:13:58 +1100

    zx await try catch

--- changeset.script.mjs
+++ changeset.script.mjs
@@ -6,5 +6,12 @@ const env = process.env.CI_ACTION_REF_NAME;
 if (env === "master") {
   await $`changeset status --output=${'changeset-out.json'} || (changeset add --empty && changeset status --output=${'changeset-out.json'})`;
 } else {
-  await $`changeset status --output=${'changeset-out.json'} --since=origin/main || (changeset add --empty && changeset status --output=${'changeset-out.json'} --since=origin/main)`;
+  // await $`changeset status --output=${'changeset-out.json'} --since=origin/main || (changeset add --empty && changeset status --output=${'changeset-out.json'} --since=origin/main)`;
+
+  try {
+    await $`changeset status --output=${'changeset-out.json'} --since=origin/main`;
+  } catch (e) {
+    await $`changeset add --empty`;
+    await $`changeset status --output=${'changeset-out.json'} --since=origin/main`;
+  }
 }
