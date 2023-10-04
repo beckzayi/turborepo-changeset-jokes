3c8cc5dec58b6e264eb71c361058ab49bc9712bc
Darren Li <darrenli@hipagesgroup.com.au>
2023-10-04 15:17:21 +1100

    publish workflow, cs-1001

--- changeset.script.mjs
+++ changeset.script.mjs
@@ -8,7 +8,7 @@
 const env = process.env.CI_ACTION_REF_NAME;
 
 // Output changeset to file
-if (env === "master") {
+if (env === "main") {
   await $`changeset status --output=${'changeset-out.json'}`;
 } else {
   // await $`changeset status --output=${'changeset-out.json'} --since=origin/main`;
